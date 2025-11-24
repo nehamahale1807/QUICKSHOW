8a0a564bc97f4bc8d79d8c8a469b0d76246f90d4
neha mahale <mahaleneha91@gmail.com>
2025-11-24 20:55:07 +0530

    Initial commit

---
+++ client/src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loading = () => {
+  return(
+    <div className='flex justify-center items-cenetr h-[80vh]'>
+        <div className='animate-spin rounded-full mt-70 h-14
+         w-14 border-2 border-t-primary'>
+        </div>
+    </div>
+  )
+}
+
+export default Loading
